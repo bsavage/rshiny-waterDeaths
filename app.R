4e734b3b7acbded595bef8a43e342f9fd75f704e
B. Savage <brian.savage@bc.edu>
2018-12-19 17:15:42 -0500

    Completion of v.1 with barplot and table

--- app.R
+++ app.R
@@ -1,14 +1,12 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# Shiny app that reads 2017 water-borne deaths by country
+# and allows user to select country and age range
+# Plots bar chart.
 #
 
 library(shiny)
 library(RCurl)
+library(ggplot2)
 
 url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTaeUxOEdnPozmASycL-5Jhv7GZ2mGLXdBYzjz9T-AsgYVgTGYA2LCQm-JoQA76HzPohusxGn_9t26x/pub?output=csv"
 csv <- getURL(url,.opts=list(ssl.verifypeer=FALSE))
@@ -19,45 +17,53 @@ countries <- unique(df$location)
 sex <- unique(df$sex)
 age <- unique(df$age)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
+  
+  # Setup title and two user input menus
+  titlePanel("Water-borne deaths by country and age (2017)"),
+  fluidRow(
+    column(4,
+           selectInput("country","Country :", 
+                       choices = countries )),
+    column(4,
+           selectInput("age","Age category :", 
+                       choices = age )),
+    br(),
 
-   # Application title
-   titlePanel("Read data sheet"),
-   # Sidebar
-   sidebarLayout(
-      sidebarPanel(
-          selectInput("country","Country :", 
-                      choices = countries ),
-        selectInput("sex","Gender :", 
-                    choices = sex ),
-        selectInput("age","Age category :", 
-                    choices = age )
-   ),
-
-      mainPanel("main panel",
-        plotOutput(outputId = "distPlot")
-      )
-   )
+    # Define are for plot and table
+    mainPanel(" ",
+              plotOutput(outputId = "barPlot"),
+              br(),
+              br(),
+              dataTableOutput('table')
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-  output$distPlot <- renderPlot({
-
-    selection <- subset(df, location==input$country & sex==input$sex & age==input$age)
-    print("selection")
-    print(selection)
-    print("hello")
-    print(selection$deaths)
-    plot(selection$country, selection$deaths, type="b",
-         xlim=range(dd$country), ylim=range(dd$deaths))
-    # hist(selection$deaths, col = "#75AADB", border = "white",
-    #      xlab = "deaths",
-    #      main = "Histogram of water")
+  
+  #Reactive data frame that will dynamically change based on what is  chosen
+  user_data <- reactive({    
     
+    #subset data frame by user selections and including only relevant columns
+    subdata = subset(df, location==input$country & age==input$age
+                     & (sex=="Male" | sex=="Female")
+                     & (cause!="All causes"))
+    
+  })
+  
+  output$barPlot <- renderPlot({
+    # Check amount of user data (send to console)
+    print(nrow(user_data()))
+  
+      ggplot(data=user_data(), aes(sex,y=deaths, fill=cause) )+
+      geom_bar(stat="identity")+xlab("Gender") + ylab("Deaths") +labs(fill = "Cause")
+
     })
+  output$table <- renderDataTable(user_data())
+                                  
 }
 
 # Run the application 
